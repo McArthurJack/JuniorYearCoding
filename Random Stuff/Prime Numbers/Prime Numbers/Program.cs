82443a4d2257adc9e21ccdeae8c4ead64db3f361
McArthurJack <132333@student.asd20.org>
2018-11-06 13:21:02 -0700

    It now finds prime numbers

--- Random Stuff/Prime Numbers/Prime Numbers/Program.cs
+++ Random Stuff/Prime Numbers/Prime Numbers/Program.cs
@@ -10,6 +10,36 @@ namespace Prime_Numbers
     {
         static void Main(string[] args)
         {
+            List<int> primeList = new List<int>();
+            int prime = 0;
+            int number = 0;
+            float numberf = number;
+            primeList.Add(2);
+            for (int i = 2; i < 200; i++)
+            {
+                for (int j = 2; j < i; j++)
+                {
+                    number = i;
+                    numberf = i;
+                    if (number/j == numberf/j)
+                    {
+
+                    }
+                    else
+                    {
+                        prime++;
+                    }
+                    if (prime == i-2)
+                        primeList.Add(i);
+                }
+                prime = 0;
+            }
+            
+            for (int i = 0; i < primeList.Count; i++)
+            {
+                Console.Write(primeList[i] + " | ");
+            }
+            Console.ReadLine();
         }
     }
 }
