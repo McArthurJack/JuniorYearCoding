497ff1d7619989cbbc070a5ffd8080375bc0a15a
McArthurJack <132333@student.asd20.org>
2018-11-08 13:29:27 -0700

    Method Overloading and decimal places

---
+++ Method Overloading/Method Overloading/Method Overloading/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int[] array = new int[5];
+            int[,] TwoArray = new int[3, 4];
+            int[][] jagged = new int[5][];
+            double f = Math.PI;
+            Print(a);
+            Print(array);
+            Print(TwoArray);
+            Print(jagged);
+            Print(f);
+            Console.ReadKey();
+        }
+
+        static void Print(int a)
+        {
+            Console.WriteLine("Integer 5");
+            Console.WriteLine();
+        }
+        
+        static void Print(int[] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                array[i] = rand.Next(0,10);
+            }
+            for (int i = 0; i < array.Length; i++)
+                Console.Write("| " + array[i] + " ");
+            Console.WriteLine("|");
+            Console.WriteLine();
+        }
+
+        static void Print(int[,] TwoArray)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    TwoArray[i, j] = rand.Next(0, 10);
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("| " + TwoArray[i, j] + " ");
+                }
+                Console.Write("|");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(int[][] jagged)
+        {
+            for (int i = 0; i < jagged.GetLength(0); i++)
+            {
+                jagged[i] = new int[i * 4];
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    jagged[i][j] = (i * j) * 3;
+                }
+            }
+            for (int i = 0; i < jagged.GetLength(0); i++)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.Write(jagged[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        static void Print(double f, float percision = 2)
+        {
+            string Val = "0.";
+            for (int i = 0; i < percision; i++)
+            {
+                Val += "#";
+            }
+            Console.WriteLine(f.ToString(Val));
+        }
+    }
+}
--- Method Overloading/Method Overloading/Method Overloading/Properties/AssemblyInfo.cs
+++ Method Overloading/Method Overloading/Method Overloading/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Prime Numbers")]
+[assembly: AssemblyTitle("Method Overloading")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prime Numbers")]
+[assembly: AssemblyProduct("Method Overloading")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("62a724d2-4003-4ff4-af12-e28d5078a49a")]
+[assembly: Guid("218a0aac-e736-40e6-9cef-64a168d9f2d5")]
 
 // Version information for an assembly consists of the following four values:
 //
