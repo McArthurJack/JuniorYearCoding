77a22b21ab021f6fc8b3e8911199f126c6fdcf88
McArthurJack <132333@student.asd20.org>
2018-11-16 14:48:02 -0700

    no message

--- Character Creation Save/Character Creation Save/Character.cs
+++ Character Creation Save/Character Creation Save/Character.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace Character_Creation_Save
 {
     class Character
     {
+        public string Name;
+        public int Strength;
+        public int Dexterity;
+        public int Happiness;
+        public int Intelligence;
+        public int Wealth;
+        public int Points = 100;
+        public int answer = 0;
+
+        public Character(string Name, int Strength, int Dexterity, int Happiness, int Intelligence, int Wealth)
+        {
+            this.Name = Name;
+            this.Strength = Strength;
+            this.Dexterity = Dexterity;
+            this.Happiness = Happiness;
+            this.Intelligence = Intelligence;
+            this.Wealth = Wealth;
+        }
     }
 }
