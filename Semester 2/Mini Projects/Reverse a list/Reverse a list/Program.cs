47b0fd8c50f8210363a4f53aec5d094a42fd33c1
McArthurJack <132333@student.asd20.org>
2019-01-29 14:39:24 -0700

    Made something that reverses a list

---
+++ Semester 2/Mini Projects/Reverse a list/Reverse a list/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Reverse = new List<int>();
+            for (int i = 1; i <= 20; i++)
+            {
+                Reverse.Add(i);
+            }
+
+            for (int i = 0; i < Reverse.Count; i++)
+                Console.Write(Reverse[i] + ", ");
+            Console.WriteLine();
+
+
+            for (int i = Reverse.Count-2; i >= 0; i--)
+            {
+                int temp = Reverse[i];
+                Reverse.RemoveAt(i);
+                Reverse.Add(temp);
+            }
+
+            for (int i = 0; i < Reverse.Count; i++)
+                Console.Write(Reverse[i] + ", ");
+            Console.ReadLine();
+        }
+    }
+}
