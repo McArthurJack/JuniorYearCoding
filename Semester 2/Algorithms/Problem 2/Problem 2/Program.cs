62c65f35080e8633c82d4137360e0f2c45d97569
McArthurJack <132333@student.asd20.org>
2019-02-05 15:56:26 -0700

    Send help

--- Semester 2/Algorithms/Problem 2/Problem 2/Program.cs
+++ Semester 2/Algorithms/Problem 2/Problem 2/Program.cs
@@ -16,29 +16,65 @@ namespace Problem_2
             string result;
             string temp;
             string temp2;
+            string temp3;
             int counter = 0;
             string path = "..\\..\\..\\..\\Problems\\" + @"Prob02.in_.txt";
             using (StreamReader sr = new StreamReader(path))
             {
                 while ((result = sr.ReadLine()) != null)
                 {
-                    Prob2.Add(sr.ReadLine());
+                    Prob2.Add(result);
                 }
             }
 
-            temp = Prob2[0];
-            for (int i = 0; i < temp.Length-1; i++)
+            for (int i = 0; i < Prob2.Count; i++)
             {
-                if (temp[i] == ' ')
+                counter = 0;
+                for (int j = 0; j < Prob2[i].Length-1; j++)
                 {
-                    counter++;
-                    temp = temp.Substring(0, counter);
-                    Work.Add(Convert.ToInt32(temp));
-                }
-                else
-                    counter++;
-            }
+                    temp2 = " ";
+                    if (temp2.Contains(' '))
+                    {
 
+                    }
+                    else
+                        break;
+                    temp = Prob2[i];
+                    if (temp[j] == ' ')
+                    {
+                        temp2 = temp.Substring(0, counter);
+                        counter++;
+                        temp = temp.Substring(counter);
+                        Work.Add(Convert.ToInt32(temp2));
+                        temp2 = "";
+                    }
+                    else
+                        counter++;
+                }
+                for (int j = 0; j < Work.Count-1; j++)
+                {
+                    if (Work[j] < Work[j+1])
+                    {
+                        counter++;
+                    }
+                    if (counter == Work.Count())
+                    {
+                        Console.WriteLine("The numbers are in ascending order");
+                    }
+                }
+                for (int j = 0; j < Work.Count-1; j++)
+                {
+                    counter = 0;
+                    if (Work[j] > Work[j + 1])
+                    {
+                        counter++;
+                    }
+                    if (counter == Work.Count())
+                    {
+                        Console.WriteLine("The numbers are in descending order");
+                    }
+                }
+            }
             Console.ReadLine();
         }
     }
