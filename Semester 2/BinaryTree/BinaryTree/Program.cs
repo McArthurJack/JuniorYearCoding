ffa5f5ad65022674e9204862320c2df7a02bab98
McArthurJack <132333@student.asd20.org>
2019-04-18 15:53:53 -0600

    Need to get the Recursive count to work.

---
+++ Semester 2/BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Insert('h');
+            tree.Insert('b');
+            tree.Insert('j');
+            tree.Insert('x');
+            tree.Insert('q');
+            tree.Insert('r');
+            tree.Insert('g');
+            tree.Print();
+            Console.WriteLine(tree.Count());
+        }
+    }
+}
